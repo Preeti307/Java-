a59866caff2990e95e3972d57295e81d56c5a7fd
Preeti Khatri <preetikhatri307@gmail.com>
2025-07-11 22:50:13 +0530

    . java

---
+++ file5.java
@@ -0,0 +1,25 @@
+// /* Problem 1 */
+// public class file5 {
+//     public static void main(String[] args){
+//         String name = "PREETI";
+//         System.out.println(name.toLowerCase());
+//     }
+// }
+
+
+// /* Problem 2 */
+// public class file5 {
+//     public static void main(String[] args){
+//         String name = "PREETI KHATRI";
+//         System.out.println(name.replace(" ","_"));
+//     }
+// }
+
+
+/* Problem 3 */
+public class file5 {
+    public static void main(String[] args){
+        String name = "Dear <|name|> , You are selected for Microsoft job , CONGRATULATION !";
+        System.out.println(name.replace("<|name|>","Preeti Khatri"));
+    }
+}
