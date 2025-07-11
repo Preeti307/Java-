fa45e52a1182fd728ca08f6574cc604e9d1b8b3a
Preeti Khatri <preetikhatri307@gmail.com>
2025-07-11 22:49:29 +0530

    . java

---
+++ file3.java
@@ -0,0 +1,82 @@
+/* Problem 1 */
+// import java.util.Scanner;
+// public class file3 {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter you subject number :");
+//         float a = s.nextFloat();
+//         float b = s.nextFloat();
+//         float c = s.nextFloat();
+//         float d = s.nextFloat();
+//         float e = s.nextFloat();
+
+//         float total_percentage = (a+b+c+d+e)/5;
+//         System.out.println("percentage = " + total_percentage);
+//     }
+// }
+
+
+/* Problem 2 */
+// import java.util.Scanner;
+// public class file3{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter you name : ");
+//         String name = s.next();
+//         System.out.println("Hello "+ name +",have a good day!");
+//     }
+// }
+
+
+/* Problem 3 */
+// import java.util.Scanner;
+// public class file3{
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter any number ,you want to convert in miles : ");
+//         int num = s.nextInt();
+//         System.out.println("km to miles = " + (num*0.621371));
+//     }
+// }
+
+
+/* Problem 4 */
+// import java.util.Scanner;
+// public class file3{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter any number: ");
+//         boolean B = s.hasNextInt();
+//         System.out.println("Is it an integer? " + B);
+//     }
+// }
+
+/* Problem 5 */
+// import java.util.Scanner;
+// public class file3{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter marks : ");
+//         float num1 = s.nextFloat();
+//         float num2 = s.nextFloat();
+//         float num3 = s.nextFloat();
+
+//         System.out.println("CGPA = " + (num1+num2+num3)/30);
+//     }
+// }
+
+
+import java.util.Scanner;
+public class file3{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println(64<65);
+
+        // 1.String sc = new String("Preeti");
+        // 2. String sc;
+        // sc = new String("Preeti");
+        String sc = s.nextLine(); // 3.
+
+        System.out.println(sc);
+    }
+}
