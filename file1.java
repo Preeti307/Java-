54e4f9066d2a39bd6466f5a7ca47afc4cc312d9c
Preeti Khatri <preetikhatri307@gmail.com>
2025-07-11 22:48:03 +0530

    . java

---
+++ file1.java
@@ -0,0 +1,35 @@
+/*  Semple code  */
+// public class file1 {
+//     public static void main(String[] args) {
+//         System.out.println("hello world");
+//     }
+// }
+
+/*  Arithamatic operation  */
+public class file1{
+    public static void main(String[] args){
+        System.out.println("Arithamatic operation");
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum (addition) of a and b is : "+ (a+b));
+        System.out.println("The subtraction of a and b is : "+ (a-b));
+        System.out.println("The subtraction of a and b is : "+ (b-a));
+        System.out.println("The multiplication of a and b is : "+ (a*b));
+        System.out.println("The division of a and b is : "+ (a/b));
+        System.out.println("The division of a and b is : "+ (b/a));
+        System.out.println("The modulo of a and b is : "+ (b%a));
+        System.out.println("The modulo of a and b is : "+ (a%b));
+        System.out.print("\n");
+
+        float c = 5.55f; // float c = 5.55F;
+        System.out.println(c);
+        char ch = 'P';
+        System.out.println(ch);
+        long lo = 124578963l; // long lo = 124578963L;
+        System.out.println(lo);
+        double dou = 12.4578963d; // double dou = 12.4578963D;
+        System.out.println(dou);
+        boolean bu = true;
+        System.out.println(bu);
+    }
+}
