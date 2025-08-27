5afa67d982547e40f58dd5e506b07568b16f65ac
Preeti Khatri <preetikhatri307@gmail.com>
2025-08-27 16:11:26 +0530

    .java

---
+++ file21.java
@@ -0,0 +1,9 @@
+import myclass.file20;
+
+public class file21 {
+    public static void main(String [] args){
+        file20 obj = new file20();
+        obj.setdata(2,3);
+        obj.getdata();
+    }
+}
