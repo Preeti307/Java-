8db6f01abfa21647d75d5459a3c22be59cbf6815
Preeti Khatri <preetikhatri307@gmail.com>
2025-08-27 16:08:49 +0530

    .java

---
+++ file20.java
@@ -0,0 +1,11 @@
+package myclass;
+public class file20 {
+    private int x,y;
+    public void setdata(int a, int b){
+        x = a;
+        y = b;
+    }
+    public void getdata(){
+        System.out.println("x = " + x + " y = " + y);
+    }
+}
